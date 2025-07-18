cb7b7e8c44acff9b4939d4498efa376028a103f8
Gong <15260819+ledoversea@user.noreply.gitee.com>
2025-07-18 23:14:00 +0800

    启用禁用员工账号

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -105,4 +105,24 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
 
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     */
+    public void startOrStop(Integer status, Long id) {
+        // 传统写法
+        /*Employee employee= new Employee();
+        employee.setStatus(status);
+        employee.setId(id);*/
+
+        // 使用builder构建器
+        Employee employee = Employee.builder()
+                .status(status)
+                .id(id)
+                .build();
+        employeeMapper.update(employee);
+    }
+
+
 }
